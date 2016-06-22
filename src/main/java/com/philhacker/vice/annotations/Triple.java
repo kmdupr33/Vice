cdb913229c02dfb044dc4ab7397e3549998a0aec
Matt Dupree <kmdupr33@gmail.com>
2016-06-22 17:18:34 -0400

    move triple outside of vice class

---
+++ src/main/java/com/philhacker/vice/annotations/Triple.java
@@ -0,0 +1,28 @@
+package com.philhacker.vice.annotations;
+
+/**
+ * Created by mattdupree on 6/22/16.
+ */
+class Triple<T, U, V> {
+    private final T t;
+    private final U u;
+    private final V v;
+
+    Triple(T t, U u, V v) {
+        this.t = t;
+        this.u = u;
+        this.v = v;
+    }
+
+    V getV() {
+        return v;
+    }
+
+    U getU() {
+        return u;
+    }
+
+    T getT() {
+        return t;
+    }
+}
--- src/main/java/com/philhacker/vice/annotations/Vice.java
+++ src/main/java/com/philhacker/vice/annotations/Vice.java
@@ -80,30 +80,6 @@ public class Vice {
         }
     }
 
-    private static class Triple<T, U, V> {
-        private final T t;
-        private final U u;
-        private final V v;
-
-        Triple(T t, U u, V v) {
-            this.t = t;
-            this.u = u;
-            this.v = v;
-        }
-
-        V getV() {
-            return v;
-        }
-
-        U getU() {
-            return u;
-        }
-
-        T getT() {
-            return t;
-        }
-    }
-
 
     private static class RecordingObject {
         private final Object rawObject;
